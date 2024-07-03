0672ad19e97025fbcb828db3fc5847bf6dfd7cc1
sanchitrathore2004 <150013490+sanchitrathore2004@users.noreply.github.com>
2024-07-04 01:41:22 +0530

    Update config.js

--- Frontend/src/utils/config.js
+++ Frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const siteUrl = 'http://localhost:8000';
+// export const siteUrl = 'http://localhost:8000';
 
-// export const siteUrl = 'https://dtunes.onrender.com';
+export const siteUrl = 'https://dtunes.onrender.com';
